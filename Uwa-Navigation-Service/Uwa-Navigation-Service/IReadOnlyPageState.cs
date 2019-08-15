59a7b864109883c66ae84e385ba3e2d146165c89
Colin Williams <colin@colincwilliams.com>
2019-08-14 22:18:57 -0700

    Added StyleCop to Uwa-Navigation-Service project

--- Uwa-Navigation-Service/Uwa-Navigation-Service/IReadOnlyPageState.cs
+++ Uwa-Navigation-Service/Uwa-Navigation-Service/IReadOnlyPageState.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="PageState.cs" company="Colin C. Williams">
+﻿// <copyright file="IReadOnlyPageState.cs" company="Colin C. Williams">
 // Copyright (c) Colin C. Williams. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
@@ -8,9 +8,9 @@ namespace ColinCWilliams.CSharpNavigationService
     using System.Collections.Generic;
 
     /// <summary>
-    /// Stores state information for a specific page.
+    /// Represents readonly PageState, which stores state information for a specific page.
     /// </summary>
-    internal class PageState : Dictionary<string, object>, IReadOnlyPageState
+    public interface IReadOnlyPageState : IReadOnlyDictionary<string, object>
     {
     }
 }
