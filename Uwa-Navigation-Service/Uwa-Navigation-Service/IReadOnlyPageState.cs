95e51c6d5e0fb79d52e247b24b5d6b084aec3c77
Colin Williams <colin@colincwilliams.com>
2019-08-23 17:47:55 -0700

    Rebuild solution (#21)
    Rebuilt the solution and all projects within to clean-up auxiliary files and ensure code analysis is using the most modern method.
    
    Also updated the name of the project to drop "CSharp" and match the name of the repository, since this library will work for all UWA's, regardless of language, despite this particular library being written in C#.

---
+++ Uwa-Navigation-Service/Uwa-Navigation-Service/IReadOnlyPageState.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IReadOnlyPageState.cs" company="Colin C. Williams">
+// Copyright (c) Colin C. Williams. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace ColinCWilliams.CSharpNavigationService
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents readonly PageState, which stores state information for a specific page.
+    /// </summary>
+    public interface IReadOnlyPageState : IReadOnlyDictionary<string, object>
+    {
+    }
+}
---
+++ Uwa-Navigation-Service/Uwa-Navigation-Service/PageState.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="PageState.cs" company="Colin C. Williams">
+// Copyright (c) Colin C. Williams. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace ColinCWilliams.CSharpNavigationService
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Stores state information for a specific page.
+    /// </summary>
+    internal class PageState : Dictionary<string, object>, IReadOnlyPageState
+    {
+    }
+}
